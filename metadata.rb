f99e1e693ae8ed23a430577b12285cb12e36423d
Kirill Shirinkin <fodojyko@gmail.com>
2017-02-24 15:06:46 +0100

    Add metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name              'citadel'
+maintainer        'Noah Kantrowitz'
+maintainer_email  'noah@coderanger.net'
+license           'Apache 2.0'
+description       'DSL for accessing secret data stored on S3 using IAM roles.'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '1.1.0'
